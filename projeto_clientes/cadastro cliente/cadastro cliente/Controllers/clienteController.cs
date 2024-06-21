79cd80750dfd8ef80bfec709d7aebbd5400d5b78
Nicolaslimaalbuquerque <nicolas.albuquerquerp@gmail.com>
2024-06-21 14:46:44 -0300

    subindo pastas

---
+++ projeto_clientes/cadastro cliente/cadastro cliente/Controllers/clienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cadastro_cliente.Controllers
+{
+    public class clienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ projeto_clientes/cadastro cliente/cadastro cliente/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cadastro_cliente.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
